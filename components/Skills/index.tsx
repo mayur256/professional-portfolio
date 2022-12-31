30fc641b4af84d37c8d837f77094c942a68331a9
Mayur Upadhayay <mayur123a@gmail.com>
2022-12-31 20:30:33 +0530

    [feat]: Adds skills carousel

--- components/Skills/index.tsx
+++ components/Skills/index.tsx
@@ -5,12 +5,15 @@ import { ReactElement } from 'react'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
 
+// Components
+import { _80 } from 'components/Meters/_80'
+
 // Component definition
 export const Skills = (): ReactElement => {
 	const responsive = {
 		superLargeDesktop: {
 			breakpoint: { max: 4000, min: 3000 },
-			items: 5,
+			items: 4,
 		},
 		desktop: {
 			breakpoint: { max: 3000, min: 1024 },
@@ -26,14 +29,31 @@ export const Skills = (): ReactElement => {
 		},
 	}
 
+	const skills = [
+		{ id: 1, name: 'web development', skillMeter: <_80 /> },
+		{ id: 2, name: 'web development', skillMeter: <_80 /> },
+		{ id: 3, name: 'web development', skillMeter: <_80 /> },
+		{ id: 4, name: 'web development', skillMeter: <_80 /> },
+		{ id: 5, name: 'web development', skillMeter: <_80 /> },
+		{ id: 6, name: 'web development', skillMeter: <_80 /> },
+	]
+
 	// Main JSX
 	return (
-		<section id="skills" className="m-8 p-8">
+		<section id="skills" className="m-8 p-8 skill-slider">
+			<div className="max-w-6xl mx-auto h-24 bg-white dark:bg-gray-800">
+				<h1 className="text-2xl md:text-4xl font-bold text-center md:text-left">
+					Skills - listed in order of competence
+				</h1>
+			</div>
+
 			<Carousel responsive={responsive}>
-				<div>Item 1</div>
-				<div>Item 2</div>
-				<div>Item 3</div>
-				<div>Item 4</div>
+				{skills.map((skill) => (
+					<div className="item" key={skill.id}>
+						{skill.skillMeter}
+						<h5 className="capitalize text-2xl">{skill.name}</h5>
+					</div>
+				))}
 			</Carousel>
 		</section>
 	)
