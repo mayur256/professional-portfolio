ecec3bdd556abf11968ad73d49bd24dca99f0f52
Mayur Upadhayay <mayur123a@gmail.com>
2023-01-06 22:45:44 +0530

    [chore]: Adding more projects

--- utils/user-data.ts
+++ utils/user-data.ts
@@ -50,6 +50,13 @@ export const projects = [
 		url: 'https://doctodoor.com/',
 	},
 	{
+		name: 'Macroplan',
+		description:
+			'Macroplan 2140™ is a blueprint for business planning consisting of business planning tools and services, to serve small and medium businesses in the 21st century.',
+		image: '/macroplan-2140.png',
+		url: 'https://macroplanfrontend.estonetech.in/',
+	},
+	{
 		name: 'RxHelp Copay/HCPay',
 		description:
 			'RxHelp ePay is a program that allows you or your clinic to be reimbursed for services you have provided to patients on behalf of a pharmaceutical manufacturer.',
@@ -62,6 +69,18 @@ export const projects = [
 			'The system is a simplistic and universal interface to access emails.The goal of the system was to achieve accessibility such that it can be used by a diverse range of people. Users are authenticated to the system via Google’s OAuth2.',
 		image: '/easymail.png',
 		url: 'https://simplegmail.plenartech.com/',
+	},
+	{
+		name: 'Weather App',
+		description: `The Weather App is an interactive application that that initially displays weather information of users' location with the help of Geolocation API. The System also allows user to query weather information of any other location in the world corresponding to its Zipcode, City, District, Province or State with data supported from Open Weather API.`,
+		image: '/weather-app.png',
+		url: 'https://weather-app-mayur256.vercel.app/',
+	},
+	{
+		name: 'Chat Application',
+		description: `A Simple chat app that facilitates peer to peer transmission of text messages over a web socket. A user can authenticate itself and chat with other users registered on the system directly.`,
+		image: '/chat_home.png',
+		url: 'https://github.com/mayur256/chat',
 	},
 	{
 		name: 'Shopnet',
