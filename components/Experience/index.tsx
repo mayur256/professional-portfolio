fde8b2fa8c92bb06e69d4af5192d32aecf4a96e9
Mayur Upadhayay <mayur123a@gmail.com>
2023-01-18 23:33:04 +0530

    [chore]: Minor adjustments in Experience and Projects

--- components/Experience/index.tsx
+++ components/Experience/index.tsx
@@ -12,13 +12,13 @@ export const Experience = (): ReactElement => {
 	return (
 		<section className="bg-white dark:bg-gray-800">
 			<div className="max-w-6xl mx-auto h-48 bg-white dark:bg-gray-800">
-				<h1 className="text-4xl md:text-8xl font-bold py-20 text-center md:text-left">
+				<h1 className="text-4xl md:text-8xl font-bold py-10 text-center md:text-left">
 					Experience
 				</h1>
 			</div>
 
 			<div className="dark:bg-gray-900">
-				<div className="grid grid-cols-1 dark:bg-gray-900 max-w-xl mx-auto pt-20">
+				<div className="grid grid-cols-1 dark:bg-gray-900 max-w-xl mx-auto md:pt-20">
 					{/** Experiences timeline */}
 					{experience.map((ex, idx) => {
 						return (
