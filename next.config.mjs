a5a6f968573474208e1a886b3ebe5ae9e8f492b9
mayur256 <39913092+mayur256@users.noreply.github.com>
2023-06-06 16:16:27 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		loader: 'akamai',
-		path: '',
-	},
-	assetPrefix: './',
+	reactStrictMode: true,
 };
 
 module.exports = nextConfig
