94c9912878b4e08c8b3ad5eece42390a065ee87d
Mayur Upadhayay <mayur123a@gmail.com>
2023-11-05 15:00:23 +0530

    feat: Integrated email reception functionality

--- components/ContactForm/index.tsx
+++ components/ContactForm/index.tsx
@@ -1,35 +1,109 @@
 // top level imports
-import { ReactElement } from 'react'
+import { FormEvent, ReactElement, useRef, useState } from 'react'
+import * as NProgress from 'nprogress';
+
+// Sweetalerts
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 // Utils
 import { user } from '@utils/user-data'
 
 // top level imports
 export function ContactForm(): ReactElement {
+    // Init
+    const MySwal = withReactContent(Swal)
+
+    // state definitions
+    const [loading, setLoading] = useState<boolean>(false);
+
+    // Ref
+    const formRef = useRef<HTMLFormElement | null>(null);
+
+    const onSubmit = async (event: FormEvent) => {
+        event.preventDefault();
+        const formData: FormData = new FormData(event.target as HTMLFormElement);
+        const payload = JSON.stringify(Object.fromEntries(formData))
+        NProgress.start();
+        setLoading(true);
+        try {
+            const apiResponse: Response = await fetch(process.env.NEXT_PUBLIC_FORMSPARK_URL ?? '', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json",
+                    accept: "application/json"
+                },
+                body: payload
+            });
+            
+            let message = "Oops! Something went wrong!";
+            let title = "Error!";
+            // await new Promise(resolve => setTimeout(resolve, 2000));
+            // const apiResponse = { status: 500 }
+            if (apiResponse?.status === 200) {
+                message = "Your requested has been submitted!";
+                title = "Success!"
+                formRef.current?.reset();
+            }
+            MySwal.fire({
+                title,
+                text: message,
+                icon: title === "Success!" ? 'success' : 'error',
+                timer: 5000
+            });
+        } finally {
+            NProgress.done();
+            setLoading(false);
+        }
+    }
+
+    // main renderer
     return (
         <div className="max-w-6xl w-full mx-auto md:grid md:grid-cols-3 md:grid-cols-3 gap-4 text-lg">
             {/** Contact form */}
             <div className="py-8 lg:py-16 px-4 mx-auto w-full col-span-2">
-                <form className="space-y-8">
+                <form className="space-y-8" onSubmit={onSubmit} ref={formRef}>
                     <div>
                         <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Name</label>
-                        <input type="text" id="name" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="John Doe" required />
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                            placeholder="John Doe"
+                            required
+                        />
                     </div>
                     <div>
                         <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                        <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="john@doe.com" required />
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                            placeholder="john@doe.com"
+                            required
+                        />
                     </div>
                     <div className="sm:col-span-2">
                         <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
-                        <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+                        <textarea
+                            id="message"
+                            name="message"
+                            rows={6}
+                            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                            placeholder="Leave a comment..."
+                            required
+                        ></textarea>
                     </div>
                     <button
-                        type="button"
+                        type="submit"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                        disabled={loading}
                     >
                         Send
                         <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
                         </svg>
                     </button>
                 </form>
